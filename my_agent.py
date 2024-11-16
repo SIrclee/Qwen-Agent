03c2a8f6b1a4c976c4e7d03e7fcdad07a230412d
SIrclee <15098126414@163.com>
2024-11-16 17:37:39 +0800

    两个agent串联测试ok

--- my_agent.py
+++ my_agent.py
@@ -1,5 +1,5 @@
 from qwen_agent.agents import Assistant
-
+from examples.my_function_calling import ragTool
 #TODO
 # Agent1：用来分析用户的输入，做规划、Plan
 # Agent2：调用工具（Rag发成OpenAI格式接口），调用Rag服务获得结果/也可以简单点把所有标志都搞在一个文件中（搞几个意思意思就行）
@@ -15,34 +15,26 @@ llm_cfg = {
 ### 第一个agent提示词
 ###
 first_instruction = '''
-你是一个综合的工具，用于从如下方面处理用户的输入内容：
-1. 识别真实意图
-2. 提取人物及其关系
-3. 以如上两点为维度，拆分子内容块
-4. 对整个输入做情感分析
+# 背景
+给你对公共标识牌进行OCR识别后的结果，你需要从去除这段文字因为OCR所产生的噪声，如果有描述有明显识别错误，进行改正
 
 用户输入如下：
 $input$
 
 按如下格式要求回复：
-意图：[用户的意图想法]
-人物关系：[人物1]-[关系]、[人物2]-[关系]
-子内容1：[拆分后的第一个子内容]
-.....
-子内容n：[拆分后的第n个子内容]
-情感分析：[情感状态]
+描述：[去噪后的公共标识描述]
 '''
 
 ###
 ### 第二个agent提示词
 ###
-second_instruction = '''
-围绕如下已知的前提条件做问询，从而了解患者的实际情况及实际问题，已知前提条件如下：
+second_instruction = '''你是一个乐于助人的AI助手。
+在收到用户的请求后，你应该：
+- 将用户的请求作为参数调用ragTool
+- 返回结果
+你只需要把ragTool的返回结果中的"fuhao"对应value输出即可。
+用户请求如下：
 $input$
-
-按如下要求回复：
-1. 以问询的方式，与用户进行沟通
-2. 必须紧扣已知前提条件进行回复
 '''
 
 while True:
@@ -52,13 +44,6 @@ while True:
     first_msg.append({'role': 'user', 'content': query})
     first_query = first_instruction.replace('$input$', query)
 
-    ###
-    ### 集合了NLP任务的AI
-    ### 1. 文本摘要
-    ### 2. 关系抽取
-    ### 3. 实体识别
-    ### 4. 情感分析
-    ### 5....
     first_agent = Assistant(
         llm=llm_cfg,
         system_message=first_query,
@@ -71,18 +56,17 @@ while True:
 
     print("\n\n-------------------------------------- agent分割线 ---------------------------------------------\n")
 
-    ###
     ### 第二个agent
-    ###
     messages = []
     second_query = second_instruction.replace('$input$', first_response[0]['content'])
     messages.append({'role': 'user', 'content': second_query})
     print(second_query)
-
+    tools = [ragTool()]
     second_agent = Assistant(
         llm=llm_cfg,
         system_message=second_query,
         name='Assistant',
+        function_list=tools,
         description='响应回复'
     )
 
